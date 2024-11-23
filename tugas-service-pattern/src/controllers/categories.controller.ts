a18bbce2d42dbcc114037aa784f9b209ad30d266
Ariq Fikri Tamamy <ariqrick@gmail.com>
2024-11-23 23:37:02 +0700

    Tugas Service Pattern

---
+++ tugas-service-pattern/src/controllers/categories.controller.ts
@@ -0,0 +1,95 @@
+import { Request, Response } from "express";
+import CategoriesModel from "../models/categories.model";
+import {create, findAll, findOne, update, remove} from "../services/categories.service"
+
+export default {
+  async create(req: Request, res: Response) {
+    try {
+      const result = await create(req.body);
+      res.status(201).json({
+        data: result,
+        message: "Success create product",
+      });
+    } catch (error) {
+      const err = error as Error;
+      res.status(500).json({
+        data: err.message,
+        message: "Failed create product",
+      });
+    }
+  },
+  async findAll(req: Request, res: Response) {
+    try {
+      const search = req.query.search;
+      const page = parseInt(req.query.page as string);
+      const limit = parseInt(req.query.limit as string);
+
+      const result = await findAll(search, limit, page);
+      res.status(200).json({
+        data: result,
+        message: "Success get all categories",
+      });
+    } catch (error) {
+      const err = error as Error;
+      res.status(500).json({
+        data: err.message,
+        message: "Failed get all categories",
+      });
+    }
+  },
+  async findOne(req: Request, res: Response) {
+    try {
+      const result = await findOne(
+        req.params?.id,
+      );
+      res.status(200).json({
+        data: result,
+        message: "Success get one product",
+      });
+    } catch (error) {
+      const err = error as Error;
+      res.status(500).json({
+        data: err.message,
+        message: "Failed get one product",
+      });
+    }
+  },
+  async update(req: Request, res: Response) {
+    try {
+      const result = await update(
+        req.params?.id,
+        req.body,
+
+      );
+
+      res.status(200).json({
+        data: result,
+        message: "Success update product",
+      });
+    } catch (error) {
+      const err = error as Error;
+      res.status(500).json({
+        data: err.message,
+        message: "Failed update product",
+      });
+    }
+  },
+  async delete(req: Request, res: Response) {
+    try {
+      const result = await remove(
+        req.params?.id,
+      );
+
+      res.status(200).json({
+        data: result,
+        message: "Success delete product",
+      });
+    } catch (error) {
+      const err = error as Error;
+      res.status(500).json({
+        data: err.message,
+        message: "Failed delete product",
+      });
+    }
+  },
+};
